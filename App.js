2d15431c433932b61459fd303db457cfc945d991
hobak12 <tjsgud2564@naver.com>
2023-01-03 23:10:13 +0900

    Feat: delete, toggle 기능 구현, button 아이콘으로 변경

--- App.js
+++ App.js
@@ -9,6 +9,8 @@ import {
 } from "react-native";
 import { useState } from "react";
 import { v4 } from "uuid";
+import { AntDesign } from "@expo/vector-icons";
+import { Feather } from "@expo/vector-icons";
 
 export default function App() {
   const [content, setContent] = useState("");
@@ -17,27 +19,55 @@ export default function App() {
   const newTodo = {
     id: v4(),
     content,
+    isDone: false,
   };
 
-  console.log(todos);
   const handleOnSubmitAddTodo = () => {
     setTodos([...todos, newTodo]);
+    setContent("");
   };
 
+  const handleOnPressDeleteTodo = (id) => {
+    const newTodos = todos.filter((todo) => todo.id !== id);
+    setTodos([...newTodos]);
+  };
+
+  const handleOnPressToggle = (id) => {
+    const newTodos = todos.map((todo) =>
+      todo.id === id ? { ...todo, isDone: !todo.isDone } : todo
+    );
+    setTodos(newTodos);
+  };
+  console.log(todos);
   return (
     <View onSubmitEditing={handleOnSubmitAddTodo} style={styles.container}>
-      <TextInput
-        style={styles.todoInput}
-        onChangeText={setContent}
-        value={content}
-      />
+      <View style={styles.todoInputWrapper}>
+        <TextInput
+          style={styles.todoInput}
+          onChangeText={setContent}
+          value={content}
+        />
+      </View>
       {todos.map((todo) => {
         return (
-          <View key={todo.id}>
-            <Text>{todo.content}</Text>
-            <Button title="삭제" />
-            <TouchableOpacity style={styles.todoEditButton}>
-              <Text>수정</Text>
+          <View key={todo.id} style={styles.todos}>
+            <Text style={todo.isDone ? styles.Done : styles.notDone}>
+              {todo.content}
+            </Text>
+            <TouchableOpacity
+              onPress={() => handleOnPressToggle(todo.id)}
+              style={styles.button}
+            >
+              <AntDesign name="checksquare" size={24} color="black" />
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => handleOnPressDeleteTodo(todo.id)}
+              style={styles.button}
+            >
+              <AntDesign name="delete" size={24} color="black" />
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.button}>
+              <Feather name="edit" size={24} color="black" />
             </TouchableOpacity>
           </View>
         );
@@ -48,17 +78,47 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
+    marginTop: 30,
     flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
   },
-  todoInput: {
-    marginTop: 50,
+  todoInputWrapper: {
     width: "90%",
+    paddingVertical: 10,
+    borderTopWidth: 1,
+    borderBottomWidth: 1,
+    marginVertical: 10,
+  },
+  todoInput: {
+    padding: 10,
     height: 50,
     backgroundColor: "aqua",
+    borderWidth: 1,
   },
-  todoEditButton: {
-    backgroundColor: "aqua",
+  todos: {
+    width: "90%",
+    flexDirection: "row",
+    backgroundColor: "lightgrey",
+    padding: 10,
+    justifyContent: "flex-end",
+    marginVertical: 5,
+  },
+  button: {
+    marginLeft: 10,
+  },
+  notDone: {
+    maxWidth: 250,
+    position: "absolute",
+    left: 10,
+    top: 10,
+  },
+
+  Done: {
+    textDecorationLine: "line-through",
+    maxWidth: 250,
+    position: "absolute",
+    left: 10,
+    top: 10,
   },
 });
