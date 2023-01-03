39ad490ee3388e88c4bbeaa61190c806c46b40f6
hobak12 <tjsgud2564@naver.com>
2023-01-03 18:53:35 +0900

    feat: todo Create, todo Read

--- App.js
+++ App.js
@@ -1,11 +1,39 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TextInput, View, Button } from "react-native";
+import { useState } from "react";
+import { v4 } from "uuid";
 
 export default function App() {
+  const [content, setContent] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const newTodo = {
+    id: v4(),
+    content,
+  };
+
+  console.log(todos);
+  const handleOnSubmitAddTodo = () => {
+    setTodos([...todos, newTodo]);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>test!</Text>
-      <StatusBar style="auto" />
+    <View onSubmitEditing={handleOnSubmitAddTodo} style={styles.container}>
+      <TextInput
+        style={styles.todoInput}
+        onChangeText={setContent}
+        value={content}
+      />
+      {todos.map((todo) => {
+        return (
+          <View key={todo.id}>
+            <Text>{todo.content}</Text>
+            <Button>
+              <Text>삭제</Text>
+            </Button>
+          </View>
+        );
+      })}
     </View>
   );
 }
@@ -15,6 +43,11 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
-    justifyContent: "center",
+  },
+  todoInput: {
+    marginTop: 50,
+    width: "90%",
+    height: 50,
+    backgroundColor: "aqua",
   },
 });
